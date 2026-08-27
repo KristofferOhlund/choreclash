f95f686ed9cc271cfc7b66b127d6b17df7a0dba3
Kristoffer <kristoffer.ohlund@icloud.com>
2026-08-27 22:42:28 +0200

    Import models globally

---
+++ src/choreclash/models/__init__.py
@@ -0,0 +1,7 @@
+from choreclash.db.db import Base, DB
+from choreclash.models.parent import Parent
+from choreclash.models.children import Child
+from choreclash.models.chore_template import ChoreTemplate
+from choreclash.models.chore2child import Chore2Child
+from choreclash.models.chore_occurence import ChoreOccurence
+from choreclash.models.chores import Chore
