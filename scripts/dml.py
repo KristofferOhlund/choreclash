354fb8a84b89d72a77a4e7e04fdd5cba1272c53a
Kristoffer <kristoffer.ohlund@icloud.com>
2026-08-27 22:44:37 +0200

    update imports

--- scripts/dml.py
+++ scripts/dml.py
@@ -1,9 +1,6 @@
-from choreclash.db.db import Base, DB
+from choreclash.db.db import DB
 from sqlalchemy.orm import Session
-from choreclash.models.chore_template import ChoreTemplate
-from choreclash.models.chore_assignment import ChoreAssignment
-from choreclash.models.parent import Parent
-from choreclash.models.children import Child
+import choreclash.models as models
 import json
 
 # Get engine object
@@ -17,10 +14,10 @@ with Session(engine) as session:
     with open('scripts/data/chores.json') as f:
         chores = json.load(f)
         for chore in chores:
-            chore_obj.append(ChoreTemplate(title=chore['title'], description=chore['description'], icon=chore['icon']))
+            chore_obj.append(models.Chore(title=chore['title'], description=chore['description'], icon=chore['icon']))
     try:
         session.add_all(chore_obj)
         session.commit()
     except Exception as e:
-        print(f"Error occurred while adding chore templates: {e}")
+        print(f"Error occurred while adding chore : {e}")
         session.rollback()
