7cf452709ec75a9d7eda8f6e15bc9df8ed7b9f81
Kristoffer Öhlund <kristoffer.ohlund@icloud.com>
2026-09-13 16:29:42 +0200

    add get children in parent_service

--- src/choreclash/api/services/parent_service.py
+++ src/choreclash/api/services/parent_service.py
@@ -1,11 +1,12 @@
 """
-Auth service module for user (parent) registration and authentication.
+Parent service module for user (parent)
 """
 
 import email
 
 from flask import session
 from choreclash.models.parent import Parent
+from choreclash.models.children import Child
 from choreclash.db.db import DB
 
 def get_parent(parent_id: str) -> Parent:
@@ -23,3 +24,16 @@ def get_parent(parent_id: str) -> Parent:
     with db.get_session() as db_session:
         parent = db_session.query(Parent).filter_by(id=parent_id).first()
         return parent
+
+def get_children(parent_id: str) -> list[Child]:
+    """
+    Retrieve the children of a parent by their ID.
+
+    Returns:
+        list[Child]: A list of Child objects corresponding to the parent's children.
+    """
+
+    db = DB()
+    with db.get_session() as db_session:
+        parent = db_session.query(Parent).filter_by(id=parent_id).first()
+        return parent.children
