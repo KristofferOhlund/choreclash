7879ec9fe20912fb69e517b720ff0df29def00e2
Kristoffer Öhlund <kristoffer.ohlund@icloud.com>
2026-09-13 16:30:51 +0200

    add child routes and service

---
+++ src/choreclash/api/routes/child_routes.py
@@ -0,0 +1,31 @@
+from flask import (
+    Blueprint, redirect, render_template, request, flash, url_for, session)
+
+from choreclash.db.db import DB
+from choreclash.models.children import Child
+from choreclash.api.services import child_service
+
+
+child_bp = Blueprint(
+    "child",
+    __name__,
+)
+
+@child_bp.route("/child/create", methods=["GET", "POST"])
+def create_child():
+    parent_id = session.get("parent_id")
+    if not parent_id:
+        flash("You must be logged in to access this page.", "error")
+        return redirect(url_for("auth.login"))
+
+    if request.method == "POST":
+        child_data = {
+            "first_name": request.form.get("first_name"),
+            "avatar_url": request.form.get("avatar_url"),
+            "parent_id": parent_id
+        }
+        child_service.create_child(child_data)
+        flash("Child created successfully.", "success")
+        return redirect(url_for("parent.canvas"))
+
+    return render_template("create_child.html")
---
+++ src/choreclash/api/services/child_service.py
@@ -0,0 +1,63 @@
+"""
+Child service module for managing children.
+"""
+from flask import session
+from choreclash.models.children import Child
+from choreclash.db.db import DB
+
+
+def get_child(child_id: str):
+    """
+    Get a child by its ID.
+
+    Args:
+        child_id (str): The ID of the child to be retrieved.
+    """
+    db = DB()
+    with db.get_session() as db_session:
+        child = db_session.query(Child).filter_by(id=child_id).first()
+        return child
+
+def update_child(chore_id: str, updated_data: dict):
+    """
+    Update a child
+
+    Args:
+        child_id (str): The ID of the child to be updated.
+        updated_data (dict): A dictionary containing the updated child data.
+    """
+    db = DB()
+    with db.get_session() as db_session:
+        child = db_session.query(Child).filter_by(id=child_id).first()
+        if child:
+            for key, value in updated_data.items():
+                setattr(child, key, value)
+            db_session.commit()
+
+def delete_child(child_id: str):
+    """
+    Delete a child
+
+    Args:
+        child_id (str): The ID of the child to be deleted.
+    """
+    db = DB()
+    with db.get_session() as db_session:
+        child = db_session.query(Child).filter_by(id=child_id).first()
+        if child:
+            db_session.delete(child)
+            db_session.commit()
+
+def create_child(child_data: dict):
+    """
+    Create a new child
+
+    Args:
+        child_data (dict): A dictionary containing the child data.
+    """
+    db = DB()
+
+    with db.get_session() as db_session:
+        new_child = Child(**child_data)
+        db_session.add(new_child)
+        db_session.commit()
